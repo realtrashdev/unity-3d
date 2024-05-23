6db67e4531ea24f741d1df7481785154c352aced
realtrashdev <mafuchoo715@gmail.com>
2024-05-23 04:40:20 -0400

    added stamina and sprint mechanic with ui element, added ambient fog, updated shack door model, fixed issue where flashlight would clip through objects, added free unity asset store trees

---
+++ 3d-testing/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    public float groundDrag;
+    public float maxStamina;
+    public float stamina;
+    public float staminaDelay;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    [HideInInspector] public bool isGrounded;
+
+    public Transform orientation;
+
+    [Header("Stamina")]
+    public StaminaBar staminaBar;
+    public GameObject staminaBarUI;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        stamina = maxStamina;
+        staminaBar.SetMaxStamina(maxStamina);
+
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        rb.drag = groundDrag;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ground check
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        CheckSprint();
+        GetInput();
+        SpeedControl();
+
+        //apply drag accordingly
+        if (isGrounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void GetInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    void MovePlayer()
+    {
+        //calculate move direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce (moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+
+    void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    void CheckSprint()
+    {
+        //if stamina exceeds the max, set it to the max & fade out stamina bar
+        if (stamina > maxStamina)
+        {
+            stamina = maxStamina;
+            staminaBarUI.GetComponent<Image>().color = Color.Lerp(staminaBarUI.GetComponent<Image>().color, new Color(0.2f, 0.28f, 0.18f, 0), Time.deltaTime * 7.5f);
+        }
+
+        //stamina goes negative, set it to 0
+        if (stamina < 0)
+        {
+            stamina = 0;
+        }
+
+        //if player fully runs out of stamina, delay stamina regen for slightly longer
+        if (stamina == 0)
+        {
+            staminaDelay = 2f;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) && staminaDelay <= 0 && rb.velocity != Vector3.zero)
+        {
+            staminaBarUI.GetComponent<Image>().color = Color.Lerp(staminaBarUI.GetComponent<Image>().color, new Color(0.2f, 0.28f, 0.18f, 1), Time.deltaTime * 15);
+
+            if (moveSpeed != 4)
+            {
+                moveSpeed += 0.025f;
+
+                if (moveSpeed > 4)
+                {
+                    moveSpeed = 4;
+                }
+            }
+
+            stamina -= Time.deltaTime;
+        }
+
+        //if not holding shift or not moving, set speed to walking and check if stamina delay is active. if not, regain stamina.
+        else if (!Input.GetKey(KeyCode.LeftShift) || rb.velocity == Vector3.zero)
+        {
+            if (stamina == 0)
+            {
+                staminaDelay = 2f;
+                stamina = 0.001f;
+            }
+
+            if (staminaDelay <= 0)
+            {
+                stamina += Time.deltaTime;
+            }
+
+            else
+            {
+                staminaDelay -= Time.deltaTime;
+            }
+
+            moveSpeed = 2;
+        }
+
+        else
+        {
+            moveSpeed = 2;
+        }
+
+        staminaBar.SetStamina(stamina);
+    }
+}
